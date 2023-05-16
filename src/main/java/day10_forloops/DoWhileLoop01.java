3100169e19cc6e0d5f23464e551bd6866a2a47b3
Demet Alkac <demetbaspinaralkac@gmail.com>
2023-05-16 21:23:12 +0200

    v3

--- src/main/java/day10_forloops/DoWhileLoop01.java
+++ src/main/java/day10_forloops/DoWhileLoop01.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class DoWhileLoop01 {
     public static void main(String[] args) {
 
-        // while -  loop ==> zero execution is possible
+        // while -  loop ==> zero execution is possible, {cünkü 1'den basladı 1 de bitti kod kırılır, 1 1'den büyük degil]
 
         int i=1;
          while (i<1){
@@ -13,7 +13,7 @@ public class DoWhileLoop01 {
              i++;
          }
 
-         // do-while
+         // do-while kullandıgımızda loop body'si icindeki kod en az bir kere calısır. do-while icin "zero execution" mümkün degil
          //When you use do-while loop, zero execution is impossible, loop body will be executed at least for once
          // If it is mandatory to execute the code inside the loop body at least once, use do while loop
         int k=1;
@@ -47,12 +47,15 @@ public class DoWhileLoop01 {
         //infinite loops
         // Note: If you do mistake in increment or decrement part. the loop will be infinite loop.
         // Do not create infinite loop
+        //Yani artırma ve azaltma kısmında hata yaparsak VEYA bu kısmı hic yazmassak "infinite loops" olusur.
+
 
         for (int p=1; p<10; p--){
 
             System.out.println(p+ " ");
         }
 
+        //NOT:Bazen "increment" yeya "decrement" kısmını koymadan kod yazarız, fakat  "break" koyarak loop'u kırarız ve sonsuz döngüden kurtarırız..
 
 
         int r=0;
@@ -65,7 +68,33 @@ public class DoWhileLoop01 {
             }
         }
 
+//   Example 1: Bir ondalık sayının ondalık kısmındaki rakamlarının toplamını bulunuz
 
+        //     24.5673  ==>5+6+7+3 = 23
+
+        double numm = 24.5673;
+        System.out.println(numm);
+
+        //String.valueOf() methodu parantezin icine konulan data nın tipine String yapar
+        String str = String.valueOf(numm);  //decimal kısmı ayırmak icin string methodlarına ihtiyacımız var.
+        System.out.println(str); //24.5673
+
+
+        //REGEX icin nokta kullandıgınızda önüne "\\" koyunuz. "\\." seklinde kullanınız.
+        String decimalPart = str.split("\\.")[1];
+        System.out.println(decimalPart); //5673
+
+       int decimalInt = Integer.valueOf(decimalPart);  // islemimize devam etmek icin (toplama-cıkarma yapabilmek icin) stringi int'e cevirdik.
+
+        int sum = 0;
+        do{
+
+            sum = sum + decimalInt%10;
+            decimalInt = decimalInt/10;
+
+        }while (decimalInt>0);
+
+        System.out.println(sum);
 
 
     }
